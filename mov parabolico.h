720ad050fb2a2d21cf2cbd38130f0debcc34b1f8
laurazpt77 <47614964+laurazpt77@users.noreply.github.com>
2019-05-17 11:33:18 -0500

    Create mov parabolico.h

---
+++ mov parabolico.h
@@ -0,0 +1,14 @@
+#ifndef mov_parbolico_H
+#define mov_parbolico_H
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+const float PI= 3.1416;       
+const float g = 9.8;          
+
+void mov_parabolico_friccion();
+
+#endif 
